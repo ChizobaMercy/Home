a8bc1ee2a7e7db0e855a450047aa59b68b95d2dc
ChizobaMercy <mercychizoba798@gmail.com>
2024-12-19 23:47:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded',()=>{
+	//toggle navigation menu
+	const navToggle= document.querySlelector(' .nav-toggle');
+	const navlinks= document.queryselector('.nav-links');
+
+	navToggle.addeventListener('click',()=>{ navLinks.classlist.toggle('nav-active');
+					       });
 	//Add event listeners to buttons
 	const hireButton=document.querySelector('.button button:nth-child(1)');
 	const cvButton=document.querySelector('.about-content button');
